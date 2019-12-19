c8f7d5b16c8bf5cd82448aa2b68b6815d2f59fde
Alexander Rank <rank.alexander@t-online.de>
2019-12-19 13:13:02 +0100

    Neue Funktionen, including getColumns, getACP, angepasste inTable und getComps. Commit für Jochen <3

--- SQLHandler.py
+++ SQLHandler.py
@@ -6,55 +6,57 @@ import sqlalchemy
 import psycopg2
 
 database="dsif"
-#database ="dax"
 user="boys"
-#user = "postgres"
 password="zaubermaus"
 host="localhost"
 #port might be 5432 for, if you haven't changed anything in the settings of postgres
 port="1997"
-directory ='/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Data Science in Finance/PortfolioProject/DAX.txt'
-#acp = adjusted closing prices
+directory ='/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Data Science in Finance/PortfolioProject/NEW/Real/DAX.txt'
+codeRegister = 'companies'
 
-tableName = 'dax'
+tableName = "acp" # A djusted C losing P rice
 #opens connecton
 con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
 
 def inTable (database, user, password, host, port, directory, existingTableName):
-
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
     #create connection, we do this here, because we might need it later connection is already created and stable the whole time
-    #con = psycopg2.connect(database = database, user =user, password= password,host= host, port =port)
 
     #read into dataframe
     #ATTENTION file has to be txt with tabstops as seperators, not a CSV file!
     #to obtain a txt form excel just save as txt (the first option if there are more than one) <3
     #Date row MUST be in the format YYYY-MM-DD please set it accordingly in excel already
     df = pd.read_csv(directory, sep="\t", error_bad_lines = False, decimal = ',')
-
+    df.columns = df.columns.str.lower()
+    df['isin'] = df['isin'].str.lower()
     #reading in the codenames so we can use them later again to match codes with names
-    #****************************NEEDS TO BE APPENDED LATER***********************************
-    codeNames = df[['Name', 'Code']]
+    #****************************Handels the Companies Names ISIN  DSCD and Index***********************************
+    codeNames = df [['name', 'isin', 'dscd', 'index']]
+    #codeNames.columns = codeNames.columns.str.lower()
+    #codeNames['isin'].str.lower()
+    print(codeNames)
+    try:
+        codeNames.to_sql(codeRegister, create_engine('postgresql://'+user+':'+password+'@'+host+':'+port+'/'+database), if_exists='append', index=False)
+    except sqlalchemy.exc.DataError:
+        print("******************** \n"
+              " Your input file is apparently in the wrong format, please check if the file is in txt with tapstop and not as CSV \n"
+              "********************")
 
-    #for now Names are better because no error appears, no leading digits in column names possible -- I will fix soon
-    df.rename(columns={'Name': 'date'}, inplace=True)
-    df = df.drop(columns='Code')
 
+    #****************************Handels the actual data, the ACP or the whatever price index****************************
+    df = df.drop(columns= ['name', 'dscd', 'index'])
     #set the index as the company names
-    df.set_index('date', inplace=True)
+    df.set_index('isin', inplace=True)
 
 
     # transpose the dataframe so it fits our database
     df = df.transpose()
 
-    #cleaning the names (column names = company names) and set them to lower case
-    # #-- get rid of withespaces and other stuff, so postgres accepts them as columns
-    df.columns = df.columns.str.replace(' ', '').str.replace('.', '').str.replace('(', '').str.replace(')', '').str.replace('-', '').str.replace("'", '').str.replace('&', '')
-    df.columns = df.columns.str.lower()
-
     #checks if the columns of the df already exists in the current table in SQL
-    existingColumns = getColumnsSQL(existingTableName)
+    #existingColumns = getColumnsSQL(existingTableName)
     #if some don't exists - add them
-    if len(set(df.columns)-set(existingColumns))!=0:
+    if False:
+            #len(set(df.columns)-set(existingColumns))!=0:
         print("in the if")
         #addColumns(existingTableName, list(existingColumns = getColumnsSQL(existingTableName)))
         print("Type of df.columns ")
@@ -62,15 +64,14 @@ def inTable (database, user, password, host, port, directory, existingTableName)
         addColumns(existingTableName, df.columns)
     #*******************HERE COMES THE MAGIC*****************************
     #creates a new table out of the dataframe called 'dax' in the database dax
-    # if it already exists, it only appends its values to the current database -- usefull for later
+     #if it already exists, it only appends its values to the current database -- usefull for later
     print("doing the df to sql")
     try:
-        df.to_sql(existingTableName, create_engine('postgresql://postgres:'+password+'@'+host+':'+port+'/'+database), if_exists='append', index=True)
+        df.to_sql(existingTableName, create_engine('postgresql://'+user+':'+password+'@'+host+':'+port+'/'+database), if_exists='append', index=True)
     except sqlalchemy.exc.DataError:
         print("******************** \n"
-              " Your input file is apparently in the wrong format, please check if the date is in the format YYYY-MM-DD and the file is in txt with tapstop not as CSV \n"
+              " Your input file is apparently in the wrong format, please check if the date is in the format YYYY-MM-DD and the file is in txt with tapstop and not as CSV \n"
               "********************")
-    print("done with dataframe to sql")
     #the previous functions is not able to realize that the index is a date, so we set it mannually here
     command = '''alter table {} 
                 alter column index 
@@ -89,9 +90,13 @@ def getColumnsSQL (tableName):
     cur.execute(command)
     fetched = cur.fetchall()
     cur.close()
-    return pd.DataFrame(fetched, columns=['ColumnNames']).values.tolist()[0]
-
+    #index out of range, hier muss noch nachgeholfen werden
+    if fetched is not []:
+        return pd.DataFrame(fetched, columns=['ColumnNames']).values.tolist()[0]
+    else:
+        return None
 def addColumns(baseTable, newColumns):
+    #new columns must be a list, basetable must be the name of the table where the columns should be added to
     #calculate the columns that are not in the current database by creating two sets and subtracting them
     # be aware that:
     # set([1, 2]) - set([2, 3])
@@ -123,21 +128,26 @@ def addColumns(baseTable, newColumns):
 
 
 #expects start date, end date and a list with company name (for now, later ticker)
-def getAP (startDate, endDate, comps):
+def getACP (startDate, endDate, comps):
+    #start date, end date, isins/code
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
     cur = con.cursor()
-    #make list of comps to string
-    compsStr = ', '.join(comps)
+    #make list of comps (ISINs) to string
+
+    compsStr = ', '.join([comp.lower() for comp in comps])
+    #compsStr = ', '.join(comps)
+    print(compsStr)
     #produces the command
     command = '''
-    select  index, {}
-    from dax
-    where index between '{}' and '{}';
-    '''.format(compsStr, startDate, endDate)
+        select index, {}
+        from acp
+        where index between '{}' and '{}';
+        '''.format(compsStr, startDate, endDate)
     #catching error if the company does not exists or is written wrongly
     try:
         cur.execute(command)
-    except psycopg2.errors.UndefinedColumn:
-        print ('psycopg2.errors.UndefinedColumn: Not a valid column name, please check!')
+    except (psycopg2.errors.UndefinedColumn,sqlalchemy.exc.DataError):
+        print ('Not a valid column name, please check!or not a valid please check if the date is in the format YYYY-MM-DD')
         return
     result = cur.fetchall()
     cur.close()
@@ -148,11 +158,35 @@ def getAP (startDate, endDate, comps):
     result = pd.DataFrame(result, columns = names)
     result.set_index('date', inplace=True)
     return result
-#******************************************************MAIN******************************************
-directory2 ='/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Data Science in Finance/PortfolioProject/AEX.txt'
-inTable(database,user, password, host, port, directory2, "dax")
-comps = ['adidas', 'adyen']
-print(getAP('2019-01-01', '2019-02-28', comps))
 
+def findComp(searchterm, columnToSearch, columnsToReturn ,assetClass):
+    #columnstoReturn must be list in ,columnToSearch and  assetClass and searchterm a strings
+    searchterm = searchterm.upper()
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cur = con.cursor()
+    selected = ', '.join([col.lower() for col in columnsToReturn])
+    command = '''
+        select {}
+        from {}
+        where {} like '%{}%';
+        '''.format(selected, assetClass, columnToSearch, searchterm)
+    #exception catch is not really necessary here, but is ok anyway
+    print(command)
+    try:
+        cur.execute(command)
+    except psycopg2.errors.UndefinedColumn:
+        print ('psycopg2.errors.UndefinedColumn: Not a valid column name, please check!')
+        return
+    result = pd.DataFrame(cur.fetchall())
+    cur.close()
+    return result
+
+#******************************************************MAIN******************************************
+#directory2 ='/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Data Science in Finance/PortfolioProject/AEX.txt'
+inTable(database,user, password, host, port, directory, tableName)
+#comps = ['DE000a1EWWW0', 'de0008404005', 'ie00bz12wp82', 'de0007100000']
+#print(getACP('2019-01-01', '2019-02-28', comps))
+#print(findComp("deutsch", "name", ["name", "index", "isin"], "companies"))
+#getColumnsSQL(tableName)
 con.commit()
 con.close()
