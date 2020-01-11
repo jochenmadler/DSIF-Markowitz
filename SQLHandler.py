4a723eb6dfca49573ee69c173781adbdc28c6340
Alexander Rank <rank.alexander@t-online.de>
2020-01-11 14:32:00 +0100

    saveUser, getUser, deleteUser und Unterfunktionoen implementiert

--- SQLHandler.py
+++ SQLHandler.py
@@ -6,12 +6,19 @@
 # funktionen saveUser und getUser sind noch nicht fertig implementiert, getUser returned momentan nur einen Dummy!!
 # hast du daten kaputt gemacht? :/ führe einfach setUP noch einmal aus
 
+# saveuser erwartet einen User als input (saveUser(User))
+# getUser erwartet die UserID als string input (getUser(userID))
+# deleteUser erwartet einen user als input (deleteUser(user))
+# ****************Bitte niemals den gleichen user (gleiche UserID ist ausschlaggebend) zweimal abspeichern ohne ihn vorher wieder zu löschen
+# -> "Sonst passieren schlimme Dinge" (Thomas Neumann)
 
 import pandas as pd
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 import sqlalchemy
 import psycopg2
 import User as us
+import OptimizeProcedure as op
+from django.db import transaction
 
 
 name = "Alex"
@@ -28,7 +35,6 @@ directory5 = ""
 directory6 = ""
 codeRegister = 'companies'
 tableName = "acp"
-counter = 0
 
 def start ():
     global database, host, user, password, port, directory, directory2, directory3, codeRegister
@@ -52,10 +58,12 @@ def start ():
         password = "zaubermaus"
         host = "localhost"
         port = "1997"
+
+
 def setUp(name):
 
     global database, host, user, password, port, directory, directory2, directory3, codeRegister
-    #remove all old relations first to avoid duplicates
+
     if name == "Jochen":
         database = "postgres"
         user = "postgres"
@@ -96,6 +104,7 @@ def setUp(name):
         directory6 = "/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Data Science in Finance/PortfolioProject/NEW/Real/HSI.txt"
         codeRegister = 'companies'
 
+    #remove all old relations
     clean()
     inTable()
     print("First import done of")
@@ -120,43 +129,83 @@ def setUp(name):
     inTable()
     print("Sixth import done of")
     print(directory)
+
     con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
     cur = con.cursor()
 
     #create the user realtion
     command = '''create table tableUsers
-    (userID varchar (80),
-    budget integer,
-    varBroker float8,
-    fixBroker float8,
+    (userID varchar (80) primary key,
+    budget float8,
+    initialBudget float8,
+    varBroker integer,
+    fixBroker integer,
     split bool,
     periodStart date,
     timeInterval char (1),
-    isinListID integer,
-    requestListID integer,
     optimizeObjective char (1));
     '''
     cur.execute(command)
     con.commit()
 
     #create the optimize request realtion
-    command = '''create table optimizeRequest
+    command = '''create table request
     (userID varchar (80),
     requestID varchar (80),
-    periodEnd date);
+    periodEnd date,
+    primary key (userID, requestID),
+    foreign key (userID) references tableUsers (userID) on delete cascade);
     '''
     cur.execute(command)
     con.commit()
-    command = '''create table isinUsers
+
+    #create the relation of isin_list for each user
+    command = '''create table isinList
         (userID varchar (80),
-        isin varchar (80));
+        isin varchar (80),
+        position integer,
+        primary key (userID , isin),
+        foreign key (userID) references tableUsers (userID) on delete cascade);
         '''
     cur.execute(command)
     con.commit()
-    command = '''create table requestResult
-            (requestID varchar (80),
-            resultID varchar (80));
-            '''
+
+    #create table results
+    command = '''create table result
+                (requestID varchar (80) ,
+                userID varchar (80) ,
+                sharpeRatio integer,
+                totalVolatility float8,
+                totalReturn float8,
+                transactionCost float8,
+                currentCapital float8, 
+                primary key (requestID, userID),
+                foreign key (userID) references tableUsers (userID) on delete cascade);
+                '''
+    cur.execute(command)
+    con.commit()
+
+    command = '''create table secweights
+                (requestID varchar (80),
+                userID varchar (80),
+                weight float8,
+                isin varchar (80),
+                postion integer,
+                primary key (requestID, isin, userID),
+                foreign key (userID) references tableUsers (userID) on delete cascade);
+                '''
+    cur.execute(command)
+    con.commit()
+
+    command = '''create table guiweights
+                    (requestID varchar (80),
+                    userID varchar (80),
+                    isin varchar (80),
+                    percent float8,
+                    amount float8,
+                    primary key (isin, requestID, userID),
+                    foreign key (userID) references tableUsers (userID) on delete cascade);
+                    '''
     cur.execute(command)
     con.commit()
     cur.close()
@@ -261,99 +310,252 @@ def findComp(searchterm, columnToSearch, columnsToReturn ,assetClass):
     cur.close()
     return result
 
-def saveRequest (optimizeRequest, counter):
+def saveRequest (optimizeRequest):
     start()
     #list mit procedures
     con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
     cur = con.cursor()
     userID = optimizeRequest.user.id
-    optimizeID = optimizeRequest.id
+    requestID = optimizeRequest.id
     periodEnd = optimizeRequest.period_end
-    #optimizeResult.security_weights.to_sql('weights', create_engine('postgresql://'+user+':'+password+'@'+host+":"+port+"/"+database), if_exists='append', index = True)
+
     command = '''
-    insert into optimzeRequest
-    values({},{},{}, {})'''.format(userID,optimizeID,periodEnd, counter)
+    insert into request
+    values('{}','{}','{}');'''.format(userID,requestID,periodEnd)
     cur.execute(command)
     con.commit()
     cur.close()
 
 def saveUser(newUser):
+
     start()
-    global counter
     con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
     cur = con.cursor()
     command = '''insert into tableusers
-    values ('{}',{},{},{},{},'{}','{}',{},{},'{}')
-    '''.format(newUser.id, newUser.budget, newUser.broker_var, newUser.broker_fix, newUser.split_shares, newUser.period_start,
-               newUser.time_interval, 1, 1, newUser.optimize_objective)
+    values ('{}',{},{},{},{},{},'{}','{}','{}');
+    '''.format(newUser.id, newUser.budget, newUser.initial_budget, newUser.broker_var, newUser.broker_fix, newUser.split_shares, newUser.period_start,
+               newUser.time_interval, newUser.optimize_objective)
     cur.execute(command)
     con.commit()
 
     #ins richtige format bringen
     #isinliste ins richtige format bringen um in relation zu speichern
+    i =0
+    for isin in newUser.ISIN_list:
+        command = '''insert into isinList
+        values ('{}','{}', {});'''.format(newUser.id, isin, i)
+        i += 1
+        cur.execute(command)
+        con.commit()
 
-    isinList = newUser.ISIN_list
-    print(isinList)
-    for isin in isinList:
-        command = '''insert into isinUsers 
-        values ('{}','{}')'''.format(newUser.id, isin)
+
+    #relation request User speichern
+    i = 0
+    for tuple in newUser.req_history:
+        req = newUser.req_history[i][0]
+        saveRequest(newUser.req_history[i][0])
+        saveResult(newUser.req_history[i][1], newUser.req_history[i][0])
+        i += 1
+
+
+def saveResult (result, request):
+    start()
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cur = con.cursor()
+
+    #saves the result to the result-relation
+    command = '''insert into result
+     values ( '{}', '{}', {}, {}, {}, {}, {});
+    '''.format(request.id, request.user.id, result.sharpe_ratio, result.total_volatility, result.total_return, result.transaction_cost, result.current_capital)
+    cur.execute(command)
+    con.commit()
+
+    #array of the security_weights into a relation
+    i=0
+    for column in result.security_weights.columns:
+        command = '''insert into secweights
+        values ('{}', '{}', {}, '{}', {});'''.format(request.id, request.user.id, result.security_weights[column][0], column, i)
+        i +=1
+        cur.execute(command)
+        con.commit()
+
+    #dataframe with gui weigths
+    for index, row in result.gui_weights.iterrows():
+        command = '''insert into guiweights 
+        values ('{}', '{}', '{}', {}, {});'''.format(request.id, request.user.id, index, row['percent_portfolio'], row['amount_eur'])
         cur.execute(command)
         con.commit()
     cur.close()
 
-    #relation requestResult speichern
-
-
-def getUser (name):
+def deleteUser (olduser):
     start()
-    return us.User("testID", 10000,['de000a1ewww0', 'de0008404005', 'de000basf111','de000a1ewww4'], optimize_objective="s", broker_fix=0)
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cur = con.cursor()
+
+    command = '''delete from tableusers 
+    where userID = '{}';'''.format(olduser.id)
+
+    cur.execute(command)
+    con.commit()
+    cur.close()
+
+def deleteUserRelations ():
+    start()
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cur = con.cursor()
+
+    command = '''drop table if exists tableusers cascade;'''
+
+    cur.execute(command)
+    con.commit()
+    cur.close()
+
+def getUser (id):
+    start()
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cur = con.cursor()
+
+    #get the user
+    command = '''select * from tableusers where userid = '{}';'''.format(id)
+    cur.execute(command)
+    con.commit()
+    result = pd.DataFrame(cur.fetchall())
+
+    #get the isin list
+    command = '''select * from isinlist where userid = '{}';'''.format(id)
+    cur.execute(command)
+    con.commit()
+    isinResult = pd.DataFrame(cur.fetchall())
+    isinList = []
+    for index, row in isinResult.iterrows():
+        isinList.append(row[1])
+
+    # set the user
+    newUser = us.User(result[0][0], budget = result[1][0], broker_var=result[3][0], broker_fix=result[4][0],
+                      period_start=result[6][0].strftime ('%Y-%m-%d'),time_interval=result[7][0], optimize_objective=result[8][0], ISIN_list=isinList)
+    newUser.initial_budget = result [2][0]
+
+    #create the reqHistory
+    # first get alle the requests
+    command = '''select * from request where userid = '{}';'''.format(id)
+    cur.execute(command)
+    con.commit()
+    requests = pd.DataFrame(cur.fetchall())
+
+    reqHistory = []
+    i =0
+    for index, row in requests.iterrows():
+        req = us.optimizeRequest
+        req.user = newUser
+        req.id = row[1]
+        req.period_end = row[2]
+        #getResult(newUser, req)
+        newUser.req_history[i][0] = req
+        newUser.req_history[i][1] = getResult(req)
+
+    return newUser
+
+def getResult (request):
+    start()
+    con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cur = con.cursor()
+
+    #get the main table of result
+    command = '''select * from result where requestid = '{}';'''.format(request.id)
+
+    cur.execute(command)
+    con.commit()
+    result = pd.DataFrame(cur.fetchall())
+    #initialise the new result
+    newResult = op.optimizeResult
+    newResult.sharpe_ratio = result[2][0]
+    newResult.total_volatility = result[3][0]
+    newResult.total_return = result[4][0]
+    newResult.transaction_cost = result[5][0]
+    newResult.current_capital = result[6][0]
+
+    #get the guiWeights
+    command = '''select * from guiweights where requestid = '{}';'''.format(request.id)
+    cur.execute(command)
+    con.commit()
+    result = pd.DataFrame(cur.fetchall())
+    result = result.drop(columns=[0,1])
+    result.rename(columns ={result.columns[0]: 'isin', result.columns[1]: 'percent_portfolio', result.columns[2]: 'amount_eur'}, inplace=True)
+    result.set_index('isin', inplace=True)
+    newResult.gui_weights = result
+
+    #get the secweigths
+    command= '''select isin, weight from secweights where requestid = '{}';'''.format(request.id)
+    cur.execute(command)
+    con.commit()
+    res = pd.DataFrame(cur.fetchall())
+    res = res.transpose()
+    res.rename(columns = res.iloc[0])
+    header = res.iloc[0,:]
+    weights = pd.DataFrame(res.iloc[1,:], index= ["isin", "weights"], columns= header)
+    newResult.security_weights = weights
+
+    return newResult
 
 def clean():
     start()
     con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
     cur = con.cursor()
     command = '''
-    drop table acp;'''
+    drop table if exists acp;'''
     try:
         cur.execute(command)
-    except psycopg2.errors.UndefinedTable:
-        print("table does not exists, doesn't matter")
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
     con.commit()
-    command = '''
-    drop table companies;'''
+    command = '''drop table if exists companies;'''
     try:
         cur.execute(command)
-    except psycopg2.errors.UndefinedTable:
-        print("table does not exists, doesn't matter")
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
     con.commit()
-    command = '''
-    drop table tableusers;'''
+    command = '''drop table if exists tableusers cascade;'''
     try:
         cur.execute(command)
-    except psycopg2.errors.UndefinedTable:
-        print("table does not exists, doesn't matter")
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
     con.commit()
-    command = '''
-    drop table optimizerequest;'''
+    command = '''drop table if exists request cascade;'''
     try:
         cur.execute(command)
-    except psycopg2.errors.UndefinedTable:
-        print("table does not exists, doesn't matter")
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
     con.commit()
-    command = '''
-        drop table isinUsers;'''
+    command = '''drop table if exists isinList;'''
     try:
         cur.execute(command)
-    except psycopg2.errors.UndefinedTable:
-        print("table does not exists, doesn't matter")
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
     con.commit()
-    command = '''
-            drop table requestResult;'''
+    command = '''drop table if exists result;'''
     try:
         cur.execute(command)
-    except psycopg2.errors.UndefinedTable:
-        print("table does not exists, doesn't matter")
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
+    con.commit()
+    command = '''drop table if exists secweights;'''
+    try:
+        cur.execute(command)
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
+    command = '''drop table if exists guiweights;'''
+    try:
+        cur.execute(command)
+    except psycopg2.errors.InFailedSqlTransaction:
+        print(" transaction failed, rollback")
+        transaction.rollback()
     con.commit()
     cur.close()
-
-
+    deleteUserRelations()
